82f813877e47069193be1028da43fbf89affaaeb
Maxim <vanvanov14@mail.ru>
2026-09-11 14:45:03 +0000

    deleted deploy scripts from tracking

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/max/django_cicd
-git pull origin master
-source .venv/bin/activate
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic
-sudo systemctl restart gunicorn
